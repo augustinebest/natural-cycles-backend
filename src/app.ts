a0221576f27172678f68509769ef92785c993f63
augustinebest <augustusbest00@gmail.com>
2022-11-03 00:27:36 +0100

    Initialized App routes and setup

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import express, { Application, Request, Response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import compression from "compression";
+import helmet from "helmet";
+import Controller from "./utils/controller.interface";
+
+class App {
+  public express: Application;
+  public port: number;
+
+  constructor(controllers: Controller[], port: number) {
+    this.express = express();
+    this.port = port;
+
+    this.initialiseDatabaseConnection();
+    this.initialiseMiddleware();
+    this.initialiseControllers(controllers);
+  }
+
+  private initialiseMiddleware(): void {
+    this.express.use(helmet());
+    this.express.use(cors());
+    this.express.use(morgan("dev"));
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+    this.express.use(compression());
+  }
+
+  private initialiseDatabaseConnection() {
+    mongoose.connect(`${process.env.PROD_DB}`);
+  }
+
+  public listen(): express.Application {
+    this.express.listen(this.port, () => {
+      console.log(`App running on PORT: ${this.port}`);
+    });
+    return this.express;
+  }
+
+  private initialiseControllers(controllers: Controller[]): void {
+    controllers.forEach((controller: Controller) => {
+      this.express.use("/api", controller.router);
+    });
+  }
+
+  public get() {
+    this.express.get("/", (req: Request, res: Response) => {
+      res.json({ message: "server is live" });
+    });
+  }
+}
+
+export default App;
